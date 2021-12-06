140bc5b619495a75a59e938c20e668ad13c14672
MichaelOrlovNeurolie <o7j665k2sqld66cyfu4m4ato6gbn4h74z5thn6obgladxyckolda>
2021-12-06 17:10:37 +0200

    test3

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
-
-
     return MaterialApp(
       title: 'Typing App',
       home: TypingPage(),
